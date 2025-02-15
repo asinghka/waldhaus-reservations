7810c7977de987908188d32e216706a280defc58
asinghka <singhkalra.arjun@gmail.com>
2025-02-15 21:28:01 +0100

    ♻️  added header component

--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -1,14 +1,11 @@
+import Header from "../components/Header.tsx";
 import MainTable from "../components/MainTable.tsx";
 
 export default function Today() {
     return (
-        <div className="py-10">
-            <header>
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <h1 className="text-3xl font-semibold tracking-tight text-gray-900">Heute am {new Date().toLocaleDateString("de-DE")}</h1>
-                </div>
-            </header>
-            <main>
+        <>
+            <Header title={"Heute am " + new Date().toLocaleDateString("de-DE")}/>
+            <main className="py-6">
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
                     <div className="sm:flex sm:items-center">
                         <div className="mt-4 sm:mt-0 sm:ml-auto sm:flex-none">
@@ -23,6 +20,6 @@ export default function Today() {
                     <MainTable/>
                 </div>
             </main>
-        </div>
+        </>
     )
 }
