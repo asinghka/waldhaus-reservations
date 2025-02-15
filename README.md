2390d804e0d8ca91bcba629d0ce404cf062afb32
asinghka <singhkalra.arjun@gmail.com>
2025-02-15 20:46:11 +0100

    🎉 initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# About
+
+This template provides a minimal setup to get React working in Vite with Electron.
+It is intended to be used as a starting point for developing beautiful desktop applications using Tailwind CSS.
+
+Technologies used in this project:
+
+* Electron
+* React
+* Vite
+* Typescript + SWC
+* Tailwind CSS
+
+
+# How to setup
+Clone this repository
+```
+git clone https://github.com/asinghka/electron-react-template.git
+```
+Install all dependencies from the project root folder
+```
+npm install
+```
+and run the electron app
+```
+npm start
+```
---
+++ electron/main.js
@@ -0,0 +1,50 @@
+import {app, BrowserWindow} from "electron";
+import {fileURLToPath} from 'url'
+import path from "node:path";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
+const isDev = !app.isPackaged;
+
+const createWindow = () => {
+    const mainWindow = new BrowserWindow({
+        width: 1920,
+        height: 1080,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    })
+
+    if (isDev) {
+        // Making sure that the Server has started before
+        // the URL is loaded
+        setTimeout(() => {
+            mainWindow.loadURL('http://localhost:5173');
+        }, 100)
+        mainWindow.webContents.openDevTools()
+    } else {
+        mainWindow.loadFile(path.join(__dirname, '..', 'dist/index.html'))
+    }
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+// Some APIs can only be used after this event occurs.
+app.whenReady().then(() => {
+    createWindow()
+
+    app.on('activate', () => {
+        // On macOS it's common to re-create a window in the app when the
+        // dock icon is clicked and there are no other windows open.
+        if (BrowserWindow.getAllWindows().length === 0) createWindow()
+    })
+})
+
+// Quit when all windows are closed, except on macOS. There, it's common
+// for applications and their menu bar to stay active until the user quits
+// explicitly with Cmd + Q.
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') app.quit()
+})
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
