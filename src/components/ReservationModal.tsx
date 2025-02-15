6e6a7343cbcef40bf083a2e17b7489ac4c546082
asinghka <singhkalra.arjun@gmail.com>
2025-02-16 00:36:41 +0100

    added temporary form input fields for modal

--- src/components/ReservationModal.tsx
+++ src/components/ReservationModal.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { Dialog, DialogBackdrop, DialogPanel, DialogTitle } from '@headlessui/react'
-import { ExclamationTriangleIcon } from '@heroicons/react/24/outline'
 
 export default function ReservationModal( { open, setOpen }: { open: boolean, setOpen: (open: boolean) => void }) {
     return (
@@ -15,22 +14,100 @@ export default function ReservationModal( { open, setOpen }: { open: boolean, se
                 <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
                     <DialogPanel
                         transition
-                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-lg data-closed:sm:translate-y-0 data-closed:sm:scale-95"
+                        className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all data-closed:translate-y-4 data-closed:opacity-0 data-enter:duration-300 data-enter:ease-out data-leave:duration-200 data-leave:ease-in sm:my-8 sm:w-full sm:max-w-3xl data-closed:sm:translate-y-0 data-closed:sm:scale-95"
                     >
                         <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
                             <div className="sm:flex sm:items-start">
-                                <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
-                                    <ExclamationTriangleIcon aria-hidden="true" className="size-6 text-red-600" />
-                                </div>
                                 <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left">
-                                    <DialogTitle as="h3" className="text-base font-semibold text-gray-900">
-                                        Deactivate account
+                                    <DialogTitle as="h3" className="text-2xl font-semibold text-gray-900">
+                                        Neue Reservierung
                                     </DialogTitle>
-                                    <div className="mt-2">
-                                        <p className="text-sm text-gray-500">
-                                            Are you sure you want to deactivate your account? All of your data will be permanently removed.
-                                            This action cannot be undone.
-                                        </p>
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="name" className="block text-lg/6 font-medium text-gray-900">
+                                                Name
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="name"
+                                                    name="name"
+                                                    type="text"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="date" className="block text-lg/6 font-medium text-gray-900">
+                                                Datum
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="date"
+                                                    name="date"
+                                                    type="text"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="time" className="block text-lg/6 font-medium text-gray-900">
+                                                Uhrzeit
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="time"
+                                                    name="time"
+                                                    type="text"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+                                        <div className="sm:col-span-4">
+                                            <label htmlFor="count" className="block text-lg/6 font-medium text-gray-900">
+                                                Anzahl
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="count"
+                                                    name="count"
+                                                    type="text"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+                                        <div className="sm:col-span-full">
+                                            <label htmlFor="contact" className="block text-lg/6 font-medium text-gray-900">
+                                                Kontaktdaten
+                                            </label>
+                                            <div className="mt-2">
+                                                <input
+                                                    id="contact"
+                                                    name="contact"
+                                                    type="text"
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-12">
+                                        <div className="col-span-full">
+                                            <label htmlFor="about" className="block text-lg/6 font-medium text-gray-900">
+                                                Anmerkungen
+                                            </label>
+                                            <div className="mt-2">
+                                                <textarea
+                                                    id="about"
+                                                    name="about"
+                                                    rows={3}
+                                                    className="block w-full rounded-md bg-white px-3 py-1.5 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 sm:text-sm/6"
+                                                    defaultValue={''}
+                                                />
+                                            </div>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -39,17 +116,17 @@ export default function ReservationModal( { open, setOpen }: { open: boolean, se
                             <button
                                 type="button"
                                 onClick={() => setOpen(false)}
-                                className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-xs hover:bg-red-500 sm:ml-3 sm:w-auto"
+                                className="inline-flex w-full justify-center rounded-md bg-blue-600 px-3 py-2 text-lg font-semibold text-white shadow-xs hover:bg-blue-500 sm:ml-3 sm:w-auto"
                             >
-                                Deactivate
+                                Speichern
                             </button>
                             <button
                                 type="button"
                                 data-autofocus
                                 onClick={() => setOpen(false)}
-                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                                className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-lg font-semibold text-gray-900 ring-1 shadow-xs ring-gray-300 ring-inset hover:bg-gray-100 sm:mt-0 sm:w-auto"
                             >
-                                Cancel
+                                Abbrechen
                             </button>
                         </div>
                     </DialogPanel>
--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -16,7 +16,7 @@ export default function Today() {
                             <button
                                 type="button"
                                 onClick={() => setOpenModal(true)}
-                                className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-lg font-regular text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                                className="block rounded-md bg-blue-600 px-3 py-2 text-center text-lg font-regular text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                             >
                                 Neue Reservierung
                             </button>
