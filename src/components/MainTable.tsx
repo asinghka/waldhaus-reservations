22986a33a34316fce31f3731fa170d58d034462b
asinghka <singhkalra.arjun@gmail.com>
2025-02-16 18:29:57 +0100

    count people and reservations from database data and filter reservations by date

--- src/components/MainTable.tsx
+++ src/components/MainTable.tsx
@@ -12,14 +12,34 @@ declare global {
     }
 }
 
-export default function MainTable() {
+export default function MainTable( { filterDate } : { filterDate: Date }) {
     const [reservations, setReservations] = useState<{ id: number; name: string; date: string; count: number; contact: string; notes: string; deleted: boolean }[]>([]);
     const [openModal, setOpenModal] = React.useState(false);
+
+    const [dayCount, setDayCount] = useState<number>(0);
+    const [dayReservation, setDayReservation] = useState<number>(0);
 
     const fetchReservations = async () => {
         try {
             const data = await window.electron.getReservations();
-            setReservations(data);
+
+            const filteredReservations = data.filter((reservation) => {
+                const reservationDate = new Date(reservation.date)
+                return reservationDate === filterDate;
+            });
+
+            setReservations(filteredReservations);
+
+            let people = 0;
+            let tables = 0;
+            for (const reservation of data) {
+                people += reservation.count;
+                tables += 1;
+            }
+
+            setDayCount(people);
+            setDayReservation(tables);
+
         } catch (error) {
             console.error("Error fetching reservations:", error);
         }
@@ -54,7 +74,7 @@ export default function MainTable() {
                     >
                         <div className="flex items-center justify-center">
                             <BookOpenIcon className="mr-2 size-6"/>
-                            2
+                            {dayReservation}
                         </div>
                     </button>
                 </div>
@@ -66,7 +86,7 @@ export default function MainTable() {
                     >
                         <div className="flex items-center justify-center">
                             <UsersIcon className="mr-2 size-6"/>
-                            6
+                            {dayCount}
                         </div>
                     </button>
                 </div>
@@ -92,7 +112,7 @@ export default function MainTable() {
                                         <th scope="col" className="px-3 py-3.5 text-xl font-semibold text-gray-900 w-1/5">
                                             Anzahl
                                         </th>
-                                        <th scope="col" className="px-3 py-3.5 text-lg font-semibold text-gray-900 w-1/5">
+                                        <th scope="col" className="px-3 py-3.5 text-lg font-semibold text-gray-900">
                                             Anmerkung
                                         </th>
                                     </tr>
@@ -100,12 +120,12 @@ export default function MainTable() {
                                     <tbody className="divide-y divide-gray-200 bg-white">
                                     {afternoonReservations
                                         .map((reservation) => (
-                                            <tr key={reservation.name} className="hover:bg-gray-50 cursor-pointer" onClick={() => setOpenModal(true)}>
+                                            <tr key={reservation.name + reservation.date} className="hover:bg-gray-50 cursor-pointer" onClick={() => setOpenModal(true)}>
                                                 <td className="py-4 pr-3 pl-4 text-xl text-center font-medium whitespace-nowrap text-gray-900 sm:pl-6">
                                                     {reservation.name}
                                                 </td>
-                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleDateString('de-DE')}</td>
-                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).getHours()}</td>
+                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleDateString('de-DE', { month: '2-digit', day: '2-digit', year: '2-digit' })}</td>
+                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hour12: false })}</td>
                                                 <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{reservation.count}</td>
                                                 <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{reservation.notes ? <div className="flex justify-center"><CheckIcon className="size-6"/></div>:''}</td>
                                             </tr>
@@ -152,12 +172,12 @@ export default function MainTable() {
                                     <tbody className="divide-y divide-gray-200 bg-white">
                                     {eveningReservations
                                         .map((reservation) => (
-                                            <tr key={reservation.name} className="hover:bg-gray-50 cursor-pointer" onClick={() => setOpenModal(true)}>
+                                            <tr key={reservation.name + reservation.date} className="hover:bg-gray-50 cursor-pointer" onClick={() => setOpenModal(true)}>
                                                 <td className="py-4 pr-3 pl-4 text-xl text-center font-medium whitespace-nowrap text-gray-900 sm:pl-6">
                                                     {reservation.name}
                                                 </td>
-                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleDateString('de-DE')}</td>
-                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).getHours()}</td>
+                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleDateString('de-DE', { month: '2-digit', day: '2-digit', year: '2-digit' })}</td>
+                                                <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{new Date(reservation.date).toLocaleTimeString('de-DE', { hour: '2-digit', minute: '2-digit', hour12: false })}</td>
                                                 <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{reservation.count}</td>
                                                 <td className="px-3 py-4 text-xl text-center whitespace-nowrap text-gray-900 w-1/5">{reservation.notes ? <div className="flex justify-center"><CheckIcon className="size-6"/></div>:''}</td>
                                             </tr>
--- src/pages/Today.tsx
+++ src/pages/Today.tsx
@@ -4,10 +4,10 @@ import MainTable from "../components/MainTable.tsx";
 export default function Today() {
     return (
         <>
-            <Header title={"Heute am " + new Date().toLocaleDateString("de-DE")}/>
+            <Header title={"Heute am " + new Date().toLocaleDateString('de-DE', { month: '2-digit', day: '2-digit', year: '2-digit' })}/>
             <main className="py-6">
                 <div className="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-                    <MainTable/>
+                    <MainTable filterDate={new Date()}/>
                 </div>
             </main>
         </>
