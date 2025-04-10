05ddb455e707aaaab04074996717999493dff02b
asinghka <singhkalra.arjun@gmail.com>
2025-04-10 15:13:13 +0200

    adjusted another reservation button height

--- src/components/ReservationTable.tsx
+++ src/components/ReservationTable.tsx
@@ -130,7 +130,7 @@ export default function ReservationTable({ filterDate } : { filterDate: Date }) 
                                 <button
                                     type="button"
                                     onClick={() => handleNewReservation()}
-                                    className="cursor-pointer ml-auto block rounded-md bg-blue-600 px-3 py-2 text-center text-lg font-regular text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
+                                    className="h-[50px] cursor-pointer ml-auto block rounded-md bg-blue-600 px-3 py-2 text-center text-lg font-regular text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600"
                                 >
                                     <div className="flex items-center justify-center">
                                         Neue Reservierung
