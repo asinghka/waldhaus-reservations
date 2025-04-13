c6ca77cf4c38de5b100cc5715ac05c1b01a7f6e2
asinghka <singhkalra.arjun@gmail.com>
2025-04-13 15:27:14 +0200

    datepicker toggles between months and years

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -63,7 +63,7 @@ export default function Stats() {
                         <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="de">
                             <DatePicker
                                 label="Monat"
-                                views={['month']}
+                                views={yearView ? ['year'] : ['month']}
                                 value={filterDate}
                                 onChange={(selectedDate) => setFilterDate(selectedDate ? selectedDate : dayjs())}
                                 slotProps={{
